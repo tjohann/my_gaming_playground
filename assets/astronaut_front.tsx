c9378add987621c23eea949d0269fce354c5303c
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2019-02-12 19:42:19 +0100

    add some more tiled examples/plagrounds

---
+++ assets/astronaut_front.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="astronaut_front" tilewidth="32" tileheight="61" tilecount="1" columns="1">
+ <image source="astronaut_front.png" width="32" height="61"/>
+</tileset>
